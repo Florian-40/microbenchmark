9168f553772bdad38ad171ac3fe9790d79d73f06
Florian Robert <florian.robert40@live.com>
2022-04-19 16:40:51 +0200

    personnalisation of autoplot

--- R/autoplot.R
+++ R/autoplot.R
@@ -4,40 +4,36 @@
 #' Uses ggplot2 to produce a more legible graph of microbenchmark timings
 #'
 #' @param object A microbenchmark object
-#' @param \dots Ignored
+#' @param expression Label of valued expression
+#' @param \dots hyperparameter of scale_y_log10 or scale_y_continuous
 #' @param log If \code{TRUE} the time axis will be on log scale.
+#' @param y_min The lower limit of the y axis (defaults to 5 percent less than
+#'   the minimum value)
 #' @param y_max The upper limit of the y axis (defaults to 5 percent more than
 #'   the maximum value)
 #' @return A ggplot2 plot
 #'
-#' @examples
-#' if (requireNamespace("ggplot2")) {
-#'
-#' tm <- microbenchmark(rchisq(100, 0),
-#'                      rchisq(100, 1),
-#'                      rchisq(100, 2),
-#'                      rchisq(100, 3),
-#'                      rchisq(100, 5), times=1000L)
-#' ggplot2::autoplot(tm)
-#' }
-#' @author Ari Friedman, Olaf Mersmann
-autoplot.microbenchmark <- function(object, ...,
+#' @author Ari Friedman, Olaf Mersmann, Florian Robert
+#' 
+autoplot.microbenchmark <- function(object, expression, 
+                                    ...,
                                     log=TRUE,
+                                    y_min=0.95*min(object$time),
                                     y_max=1.05 * max(object$time)) {
-  if (!requireNamespace("ggplot2"))
+  if (!requireNamespace("ggplot2")){
     stop("Missing package 'ggplot2'.")
-  y_min <- 0
-  object$ntime <- convert_to_unit(object$time, "t")
-  plt <- ggplot2::ggplot(object, ggplot2::aes_string(x="expr", y="ntime"))
-  plt <- plt + ggplot2::coord_cartesian(ylim=c(y_min , y_max))
-  plt <- plt + ggplot2::stat_ydensity()
-  plt <- plt + ggplot2::scale_x_discrete(name="")
-  y_label <- sprintf("Time [%s]", attr(object$ntime, "unit"))
-  plt <- if (log) {
-    plt + ggplot2::scale_y_log10(name=y_label)
-  } else {
-    plt + ggplot2::scale_y_continuous(name=y_label)
   }
-  plt <- plt + ggplot2::coord_flip()
+  levels(object$expr)<-expression
+  object$ntime <- convert_to_unit(object$time, "ms")
+  plt <- ggplot2::ggplot(object)
+  plt<-plt+ggplot2::stat_ydensity(ggplot2::aes(x=expr, y=ntime, fill=expr), show.legend = FALSE)
+  plt<- plt+ ylim(y_min , y_max)
+  plt <- plt + ggplot2::scale_x_discrete(name="Valued expression")
+  y_label <- sprintf("Temps de calcul [%s]", attr(object$ntime, "unit"))
+  plt <- if (log) {
+    plt + ggplot2::scale_y_log10(name=y_label, ...)
+  } else {
+    plt + ggplot2::scale_y_continuous(name=y_label, ...)
+  }
   plt
 }
---
+++ src/config.h
@@ -0,0 +1,77 @@
+/* src/config.h.  Generated from config.h.in by configure.  */
+/* src/config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/time.h> header file. */
+#define HAVE_SYS_TIME_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* clockid_t to use with clock_gettime */
+/* #undef MB_CLOCKID_T */
+
+/* Define to 1 if you have the `clock_gettime` function. */
+/* #undef MB_HAVE_CLOCK_GETTIME */
+
+/* Define to 1 if you have the `gethrtime` function. */
+/* #undef MB_HAVE_GETHRTIME */
+
+/* Define to 1 if you have the `gettimeofday` function. */
+/* #undef MB_HAVE_GETTIMEOFDAY */
+
+/* Define to 1 if you have the `mach_absolute_time` function. */
+#define MB_HAVE_MACH_TIME 1
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "https://github.com/joshuaulrich/microbenchmark/issues"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "microbenchmark"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "microbenchmark VERSION"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "microbenchmark"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "VERSION"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Define for Solaris 2.5.1 so the uint64_t typedef from <sys/synch.h>,
+   <pthread.h>, or <semaphore.h> is not used. If the typedef were allowed, the
+   #define below would cause a syntax error. */
+/* #undef _UINT64_T */
+
+/* Define to the type of an unsigned integer type of width exactly 64 bits if
+   such a type exists and the standard includes do not define it. */
+/* #undef uint64_t */
