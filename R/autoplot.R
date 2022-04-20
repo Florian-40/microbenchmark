c9b47c1b80eefd028a6e8875f23de107525870ee
Florian Robert <florian.robert40@live.com>
2022-04-20 21:38:33 +0200

    modification des breaks et minor_breaks autoplot

--- R/autoplot.R
+++ R/autoplot.R
@@ -5,17 +5,20 @@
 #'
 #' @param object A microbenchmark object
 #' @param expression Label of valued expression
-#' @param \dots hyperparameter of scale_y_log10 or scale_y_continuous
+#' @param breaks break of scale_y_log10 if \code{log=TRUE}. 
+#' @param \dots hyperparameter of  scale_y_continuous
 #' @param log If \code{TRUE} the time axis will be on log scale.
 #' @param y_min The lower limit of the y axis (defaults to 5 percent less than
 #'   the minimum value)
 #' @param y_max The upper limit of the y axis (defaults to 5 percent more than
 #'   the maximum value)
 #' @return A ggplot2 plot
+#' @import scales
 #'
 #' @author Ari Friedman, Olaf Mersmann, Florian Robert
 #' 
 autoplot.microbenchmark <- function(object, expression, 
+                                    breaks=c(0,1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1e0, 1e1, 1e2, 1e3, 1e4, 1e5),
                                     ...,
                                     log=TRUE,
                                     y_min=0.95*min(object$time),
@@ -31,8 +34,14 @@ autoplot.microbenchmark <- function(object, expression,
   plt <- plt + ggplot2::scale_x_discrete(name="Valued expression")
   y_label <- sprintf("Temps de calcul [%s]", attr(object$ntime, "unit"))
   plt <- if (log) {
-    plt + ggplot2::scale_y_log10(name=y_label, ...)
-  } else {
+    minor<-c()
+    for (i in 1:length(breaks)){
+      minor <- c(minor, seq(breaks[i],breaks[i]*10,length.out=10))
+    }
+    plt + ggplot2::scale_y_log10(name=y_label, 
+                                 breaks=c(0,1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1e0, 1e1, 1e2, 1e3, 1e4, 1e5),
+                                 minor_breaks=minor)
+    } else {
     plt + ggplot2::scale_y_continuous(name=y_label, ...)
   }
   plt
